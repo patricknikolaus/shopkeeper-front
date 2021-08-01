c43a080c754b4750a78b4f54d808ba864219d9f4
Patrick <pdn6991@gmail.com>
2021-08-01 17:07:02 -0400

    creates vue app and tests frontend/backend connection

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <div v-for="game in games">
+      <p><img v-bind:src="game.image_url"></p>
+      <h1>{{ game.title }}</h1>
+      <p>{{ game.details }}</p>
+      <p>{{ game.rating }}/10</p>
+      <hr>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style></style>
 
+<script>
+import axios from "axios";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function () {
+    return {
+      message: "Welcome to Shopkeep.gg",
+      games: [],
+    };
+  },
+  created: function () {
+    this.gameIndex();
+  },
+  methods: {
+    gameIndex: function () {
+      axios.get("/games").then((response) => {
+        console.log(response.data);
+        this.games = response.data;
+      });
+    },
+  },
+};
 </script>
